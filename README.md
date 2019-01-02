3b2eb267705666a8bedc4c40154a2da1be912118
trackpete <github@pwx.me>
2019-01-02 13:02:08 -0500

    first pass of code to prep data into local sql db

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 I wanted to summarize some information about the books I've read,
 which leads us to this. It's an experimental work in progress at
 this moment and I've no idea if it'll work for anyone else. ;)
+
+# Notes
+
+1. the reviews list API seems to be the only effective way to get books from my read shelf
+2. the API is XML and can be parsed into an object with xml2js but there are arrays as a result
+3. the API is missing a *ton* of information, has fields for stuff like page numbers/etc. but they aren't populated
+4. shelves API doesn't seem to return ASIN or any category information on the books. Perusing goodreads APIs, it seems like they don't offer any good detailed book information.
+5. Going to need to find a way to look up individual book data on Amazon for categories/etc. Probably want Amazon rating anyway.
+6. https://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_GettingStarted.html this is kinda bonkers and excessive to just want to get some book information.
+7. Signed up to affiliate program, now they say "Start advertising on your site: You must drive at least three qualified sales within 180 days in order to avoid termination of your Associate account."
+"Once we have emailed you confirming your application has been reviewed and you have been accepted into the Associates Program, you will be able to access PA API. " Thanks. Great. So I can't even use this.
+8. review.book.id = goodreads ID. Send this to book show API (i.e. https://www.goodreads.com/book/show/42969396.xml) and it returns kindle ASIN. https://amazon.com/dp/ASIN gives you the Amazon web page.
+9. Since the goodreads API is extremely rate limited, I need to make sure to do these calls synchronously with a delay. Wee. This is one of the minor annoyances I find working with node sometimes, even though usually async is way better.
+10. Decided to change my methodology a bit - start by focusing on pulling in data and saving it, then worry about processing it later from a db. 1s delay between requests will waste too much time to dev + tweak on realtime data.
+
---
+++ prep-data.js
@@ -0,0 +1,124 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const parseString = require("xml2js").parseString;
+const sqlite3 = require("sqlite3").verbose();
+const async = require("async");
+
+const db = new sqlite3.Database("cache/localdb.sqlite");
+const ShelfURL =
+  "https://www.goodreads.com/review/list/" +
+  process.env.GOODREADS_USER_ID +
+  ".xml";
+
+// Here's a basic overview of what we're doing here:
+//
+// 1. Get reviews for the env specified shelf ID from the goodreads API
+//    a. Store a copy of the full shelf response in XML and JSON in sqlite
+//    b. Extract the GR ID from each item in the shelf to do #2
+// 2. For each book on the shelf, with a delay, hit the GR Book API for that book
+//    a. Store a copy of the book API response in XML and JSON
+//    b. Extract the ASIN from the book API response to do #3
+// 3. Make an Amazon request to get the web page for this ASIN
+//    a. Save an HTML copy in the db
+//
+// This overall populates or re-populates the database with a bunch of rawish data.
+// A separate script will then process the rawish data from the DB so I don't have to
+// keep calling out to APIs and web pages while tweaking it. From a practical end/final
+// use perspective, this is more complicated than it needs to be but it's much more
+// convenient from a dev perspective.
+//
+// (it may be worth noting this isn't intended to store data in a way to track updates,
+// only fetch deltas, etc. - it's gonna be run very rarely so just builds everything)
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS responseData (name TEXT PRIMARY KEY, source TEXT, rawData TEXT, jsData TEXT)"
+);
+
+axios
+  .get(ShelfURL, {
+    params: {
+      key: process.env.GOODREADS_API_KEY,
+      shelf: "read",
+      per_page: "200",
+      v: "2"
+    }
+  })
+  .then(function(response) {
+    // convert from XML to JS
+    parseString(response.data, function(err, jsonResponse) {
+      db.run(
+        "INSERT OR REPLACE INTO responseData (name, source, rawData, jsData) VALUES (?, ?, ?, ?)",
+        [
+          "shelfData",
+          "gr_shelf_api",
+          response.data,
+          JSON.stringify(jsonResponse)
+        ]
+      );
+      
+      // need this to be synchronous to avoid spamming the GR API
+      //jsonResponse.GoodreadsResponse.reviews[0].review.forEach(review => {
+      async.eachLimit(jsonResponse.GoodreadsResponse.reviews[0].review, 1, function(review, callback) {
+        // Get goodreads ID to send to API for lookup
+        var thisGID = review.book[0].id[0]._;
+        var thisBookURL =
+          "https://www.goodreads.com/book/show/" + thisGID + ".xml";
+        delayedGoodreadsBookLookup(thisBookURL, thisGID, callback);
+      });
+      //console.log(JSON.stringify(jsonResponse.GoodreadsResponse.reviews, null, 2));
+    });
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+  .then(function() {
+    // always executed, saving for later
+  });
+
+async function delayedGoodreadsBookLookup(thisBookURL, thisGID, callback) {
+  console.log(" *BOOKAPI:* Will fetch information shortly for:", thisBookURL);
+  await delay(2000);
+  axios
+    .get(thisBookURL, {
+      params: {
+        key: process.env.GOODREADS_API_KEY
+      }
+    })
+    .then(function(response) {
+      parseString(response.data, function(err, jsonResponse) {
+        db.run(
+          "INSERT OR REPLACE INTO responseData (name, source, rawData, jsData) VALUES (?, ?, ?, ?)",
+          [thisGID, "gr_book_api", response.data, JSON.stringify(jsonResponse)]
+        );
+        var thisASIN = jsonResponse.GoodreadsResponse.book[0].kindle_asin[0];
+        console.log("          > Kindle ASIN:", thisASIN);
+        var thisAmazonURL = "https://www.amazon.com/dp/" + thisASIN;        
+        axios.get(thisAmazonURL, {})
+        .then(function(response) {
+            console.log("          > Saving", thisAmazonURL);
+            //console.log(response.data);
+            db.run(
+                "INSERT OR REPLACE INTO responseData (name, source, rawData) VALUES (?, ?, ?)",
+                [thisASIN, "amz_dp", response.data]
+              );            
+
+        }).catch(function(error) {
+          console.log(error);
+       }).then(function() {
+        callback();
+      });
+
+
+
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
+}
+
+async function delay(msec) {
+  return new Promise(resolve => setTimeout(resolve, msec));
+}
